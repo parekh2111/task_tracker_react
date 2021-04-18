ad0d1fe37fdf543c85786a15d81fa01b1578080a
parekh2111 <parekh2111@gmail.com>
2021-04-18 16:24:23 +0200

    Create Task List , add Taks button ,

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Header from './component/Header';
+import Task from './component/Tasks';
+function App () {
+  	const [tasks, setTasks] = useState([
+		{
+			id: 1,
+			text: 'learning React',
+			day: '19th April',
+			reminder: true,
+		},
+		{
+			id: 2,
+			text: 'learning C# & C++ ',
+			day: '20th April',
+			reminder: true,
+		},
+		{
+			id: 3,
+			text: 'learning Java',
+			day: '21th April',
+			reminder: true,
+		},
+	]);
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <Header />
+      <Task tasks={tasks}/>
     </div>
   );
 }
